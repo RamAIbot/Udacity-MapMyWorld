a2f0e62119c2af6fe852aa9799c3dacdf9385f8a
RamAIbot <ramachandranss22@gmail.com>
2020-08-25 12:04:47 +0000

    MapMyWorld Project V1.0

--- devel/local_setup.sh
+++ devel/local_setup.sh
@@ -3,7 +3,7 @@
 
 # since this file is sourced either use the provided _CATKIN_SETUP_DIR
 # or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/workspace/whereami/Udacity-WhereamI/devel}
+: ${_CATKIN_SETUP_DIR:=/home/workspace/MapMyWorld/Udacity-WhereamI/devel}
 CATKIN_SETUP_UTIL_ARGS="--extend --local"
 . "$_CATKIN_SETUP_DIR/setup.sh"
 unset CATKIN_SETUP_UTIL_ARGS
--- devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
+++ devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
@@ -1,2 +1,2 @@
 set(ball_chaser_MESSAGE_FILES "")
-set(ball_chaser_SERVICE_FILES "/home/workspace/whereami/Udacity-WhereamI/src/ball_chaser/srv/DriveToTarget.srv")
+set(ball_chaser_SERVICE_FILES "/home/workspace/MapMyWorld/Udacity-WhereamI/src/ball_chaser/srv/DriveToTarget.srv")
